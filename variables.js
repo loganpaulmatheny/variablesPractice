d2ecdf4d8387d3622de69655414b96d465ee016b
Logan Matheny <loganpaulmatheny@gmail.com>
2023-06-16 11:35:01 -0700

    Add examples of various data types.

---
+++ variables.js
@@ -0,0 +1,11 @@
+var name = "Logan";
+var sport = "soccer";
+var food = "pizza";
+
+var age = 30;
+var wins = 5;
+var numSlices = 8;
+
+var boy = true;
+var enjoySport = true;
+var isHungry = false;
