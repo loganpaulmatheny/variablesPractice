064d5366b4de075b561b0bdb0752e52fd7694bbb
Logan Matheny <loganpaulmatheny@gmail.com>
2023-06-16 11:40:23 -0700

    Update variable examples and create a print statement within the variable.js file.

--- variables.js
+++ variables.js
@@ -9,3 +9,11 @@ var numSlices = 8;
 var boy = true;
 var enjoySport = true;
 var isHungry = false;
+
+name = "Arlo";
+sport = "chasing bunnies";
+food = "bones";
+
+console.log(
+  `${name} likes playing his favorite sport which is ${sport} after he eats his favorite food which is ${food}.`
+);
